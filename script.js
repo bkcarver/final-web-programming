3d88f6a3e951766bc99181a7a405c0e77ba1af22
bkcarver <bkcarver@gmail.com>
2019-05-08 22:18:02 -0400

    initial commit
    intial form build

---
+++ script.js
@@ -0,0 +1,13 @@
+var vm = new Vue ({
+  el: "#alpaca-registration",
+  data: {
+    firstName: "",
+    lastName: "",
+    address: "",
+    state: null,
+    country: "",
+    amount: [],
+    other: "",
+    submitForm: null
+  }
+});
